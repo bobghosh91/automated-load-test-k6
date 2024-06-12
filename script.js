f2dd56bd61a105c8016a5a2035e48decc6b3ba66
globant-bob-ghosh <160033278+globant-bob-ghosh@users.noreply.github.com>
2024-06-13 00:08:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+import { sleep } from 'k6';
+import http from 'k6/http';
+
+export const options = {
+    vus: 10,
+    duration: '5s'
+}
+
+export default function(){
+
+    http.get('https://uat.phactmi.org/search');
+    sleep(1)
+}
